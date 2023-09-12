dfb083b64b90074eaaaf134f6870dee2fb1b9ffa
Ysb321 <138311498+Ysb321@users.noreply.github.com>
2023-09-12 14:04:02 +0530

    Update repo.py

--- jskgd/repo.py
+++ jskgd/repo.py
@@ -4,7 +4,7 @@ repo_url = 'https://github.com/AUTOMATIC1111/stable-diffusion-webui'
 
 # Run the git clone command
 try:
-    subprocess.run(['git', 'clone', repo_url], check=True)
+    subprocess.run(['git', 'clone', repo_url])
     print('Clone operation completed successfully! 😄')
 except subprocess.CalledProcessError:
     print('Clone operation failed! 😞')
